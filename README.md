35f1414d9ea0ade73f3ee40f2e228ec5c0119c3b
Pablo Rivera <pablomrivera@outlook.com>
2024-01-06 12:33:27 -0700

    added functions to write file and initialize app

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # README-Generator
+
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+  ## Description
+
+  A console application that will ask a series of questions about you project and use the answeres provided to generate a proffessional README md file
+
+  ## Table of Contents
+  * [Installation](#Installation)
+  * [Usage](#Usage)
+  * [License](#License)
+  * [Contributing](#Contributing)
+  * [Test](#Test)
+  * [Questions](#Questions)
+  ## Installation
+
+  node index.js
+
+  ## Usage
+
+  N/A
+
+  ## License
+
+  This project is covered under the MIT License
+
+  ## Contributing
+
+  N/A
+
+  ## Test
+
+  npm test
+
+  ## Questions
+  
+  For any questions please visit my Github at <https://github.com/undefined> or email me at <pablomrivera@outlook.com>
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,5 +1,4 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
+// function for rendering license badge based on whats selected in questions
 function renderLicenseBadge(license) {
   switch (license) {
     case 'Apache License 2.0':
@@ -35,40 +34,50 @@ function renderLicenseBadge(license) {
   }
 }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
+// function to generate the md with the answeres provided fron inquirer
 function generateMarkdown(data) {
+
+  const licenseBadge = renderLicenseBadge(data.license);
+
   return `# ${data.title}
-  ${badge}
+
+  ${licenseBadge}
+
   ## Description
+
   ${data.description}
+
   ## Table of Contents
-    * [Installation](#Installation)
-    * [Usage](#Usage)
-    * [License](#License)
-    * [Contributing](#Contributing)
-    * [Test](#Test)
-    * [Questions](#Questions)
+  * [Installation](#Installation)
+  * [Usage](#Usage)
+  * [License](#License)
+  * [Contributing](#Contributing)
+  * [Test](#Test)
+  * [Questions](#Questions)
   ## Installation
+
   ${data.installation}
+
   ## Usage
+
   ${data.usage}
+
   ## License
 
+  This project is covered under the ${data.license}
+
   ## Contributing
+
   ${data.contributing}
+
   ## Test
+
   ${data.test}
+
   ## Questions
-  ${data.questions}
+  
+  For any questions please visit my Github at <https://github.com/${data.username}> or email me at <${data.email}>
 `;
 }
 
-module.exports = generateMarkdown;
+module.exports = {generateMarkdown};
